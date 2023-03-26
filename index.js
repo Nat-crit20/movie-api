79cd343d99a39fabe3b9a8925a143f16528b68fc
Nat-crit20 <greuelnathan74@gmail.com>
2023-03-26 14:58:58 -0500

    Added password hashing for updating user

--- index.js
+++ index.js
@@ -155,12 +155,13 @@ app.put(
   ],
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
+    const hashedPassword = Users.hashPassword(req.body.Password);
     Users.findOneAndUpdate(
       { _id: req.params.id },
       {
         $set: {
           Username: req.body.Username,
-          Password: req.body.Password,
+          Password: hashedPassword,
           Email: req.body.Email,
           Birthday: req.body.Birthday,
         },
